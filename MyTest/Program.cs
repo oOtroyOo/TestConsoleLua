ff36eb3099e3ab5961239cf10766f0f6da38c13e
oOtroyOo <497790325@qq.com>
2024-12-13 14:44:40 +0800

    安师大

--- MyTest/Program.cs
+++ MyTest/Program.cs
@@ -15,6 +15,14 @@ class Program
             var code = File.ReadAllText("NewFile.lua", Encoding.UTF8);
 
             Console.WriteLine(luaEnv.DoString(code).First());
+
+            code = "return function(str)\n" +
+                   "  print(str)\n" +
+                   "  return '天天都要旺'\n" +
+                   "end";
+
+            LuaFunction func = luaEnv.DoString(code).First() as LuaFunction;
+            Console.WriteLine(func.Func<string, string>("今年旺不旺"));
         }
 
         if (!Debugger.IsAttached)
