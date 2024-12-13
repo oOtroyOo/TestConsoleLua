9c3b6bf22e86f3316653d547f17ab880b5f6bcee
oOtroyOo <497790325@qq.com>
2024-12-13 12:48:58 +0800

    11111

---
+++ MyTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using XLua;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (LuaEnv luaEnv = new LuaEnv())
+        {
+            var code =
+                "print('今年一定旺') \n" +
+                "return '明年也要旺'";
+
+            Console.WriteLine(luaEnv.DoString(code).First());
+        }
+    }
+}
